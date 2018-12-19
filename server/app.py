ca9caa9d046e7b9d4db49cfe0e4c6d4200ac7f72
Pavel Rebrov <git@0dd.me>
2018-12-19 10:46:02 +0800

    Add comments

--- server/app.py
+++ server/app.py
@@ -14,8 +14,13 @@ load_dotenv(find_dotenv(), override=True)
 # Your Account Sid and Auth Token from twilio.com/console
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+# Twilio number to send SMS from and make calls from (CallerId and SenderId).
+# Purchase a number that supports both SMS and Voice from https://www.twilio.com/console/phone-numbers/search
 TWILIO_NUMBER = os.getenv('TWILIO_NUMBER')
+# Number to send WhatsApp messages from.
+# For WhatsApp Sandbox use +14155238886. To receive messages, recipent must join Sandbox as instructed on https://www.twilio.com/console/sms/whatsapp/learn
 TWILIO_WHATSAPP_NUMBER = os.getenv('TWILIO_WHATSAPP_NUMBER')
+# Twilio Studio Flow Sid from https://www.twilio.com/console/studio/dashboard
 TWILIO_FLOW_SID = os.getenv('TWILIO_FLOW_SID')
 
 # Construct a Flow REST API URL
@@ -37,7 +42,15 @@ def outbound_studio_flow(to_sms,
                          to_whatsapp,
                          to_email,
                          message_body):
-    # Construct data to send
+    """Send a Twilio Studio POST request.
+
+    Keyword arguments:
+    to_sms -- phone number to send SMS to (E.164) 
+    to_voice -- phone number to place voice call to (E.164)
+    to_whatsapp -- phone number to send WhatsApp to (E.164)
+    to_email -- email address to send email to (E.164)
+    message_body -- message body (will be spoken out as text-to-speech when placing voice call)
+    """
     data = {
         'To': to_voice,
         'From': TWILIO_NUMBER,
@@ -66,7 +79,7 @@ def outbound_studio_flow(to_sms,
             print("HTTP Request failed")
 
 
-# Submit notifications
+# HTTP endpoint to submit notification to
 @app.route('/notifications', methods=['POST'])
 def notify():
     response_object = {'status': 'success'}
